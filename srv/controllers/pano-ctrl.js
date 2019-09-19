fdc5725b12b26b8f538030ac37219dd1e96bc11e
Chenglu Li <kabelee92@gmail.com>
2019-09-18 21:58:05 -0500

    Use nodemon and morgan | chores

--- srv/controllers/pano-ctrl.js
+++ srv/controllers/pano-ctrl.js
@@ -1,186 +1,181 @@
-const Pano = require('../models/pano-db-model')
+const Pano = require("../models/pano-db-model");
 
-createPano = (req, res) => {
-    const body = req.body
+const createPano = (req, res) => {
+	const body = req.body;
 
-    if (!body) {
-        return res.status(400).json({
-            success: false,
-            error: 'You must provide a pano',
-        })
-    }
+	if (!body) {
+		return res.status(400).json({
+			success: false,
+			error: "You must provide a pano"
+		});
+	}
 
-    const pano = new Pano(body)
+	const pano = new Pano(body);
 
-    if (!pano) {
-        return res.status(400).json({ success: false, error: err })
-    }
+	if (!pano) {
+		return res.status(400).json({ success: false, error: err });
+	}
 
-    pano
-        .save()
-        .then(() => {
-            return res.status(201).json({
-                success: true,
-                id: pano._id,
-                message: 'Pano created!',
-            })
-        })
-        .catch(error => {
-            return res.status(400).json({
-                error,
-                message: 'Pano not created!',
-            })
-        })
-}
+	pano
+		.save()
+		.then(() => {
+			return res.status(201).json({
+				success: true,
+				id: pano._id,
+				message: "Pano created!"
+			});
+		})
+		.catch(error => {
+			return res.status(400).json({
+				error,
+				message: "Pano not created!"
+			});
+		});
+};
 
-deletePano = async (req, res) => {
-    await Pano.findOneAndDelete({ _id: req.params.id }, (err, pano) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+const deletePano = async (req, res) => {
+	await Pano.findOneAndDelete({ _id: req.params.id }, (err, pano) => {
+		if (err) {
+			return res.status(400).json({ success: false, error: err });
+		}
 
-        if (!pano) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Pano not found` })
-        }
+		if (!pano) {
+			return res.status(404).json({ success: false, error: `Pano not found` });
+		}
 
-        return res.status(200).json({ success: true, data: pano })
-    }).catch(err => console.log(err))
-}
+		return res.status(200).json({ success: true, data: pano });
+	}).catch(err => console.log(err));
+};
 
-getPanoById = async (req, res) => {
-    await Pano.findOne({ id: req.params.id }, (err, pano) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+const getPanoById = async (req, res) => {
+	await Pano.findOne({ id: req.params.id }, (err, pano) => {
+		if (err) {
+			return res.status(400).json({ success: false, error: err });
+		}
 
-        if (!pano) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Pano not found` })
-        }
-        return res.status(200).json({ success: true, data: pano })
-    }).catch(err => console.log(err))
-}
+		if (!pano) {
+			return res.status(404).json({ success: false, error: `Pano not found` });
+		}
+		return res.status(200).json({ success: true, data: pano });
+	}).catch(err => console.log(err));
+};
 
-getPanoFileNameById = async (req, res) => {
-    await Pano.findOne({ id: req.params.id }, (err, pano) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+const getPanoFileNameById = async (req, res) => {
+	await Pano.findOne({ id: req.params.id }, (err, pano) => {
+		if (err) {
+			return res.status(400).json({ success: false, error: err });
+		}
 
-        if (!pano) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Pano not found` })
-        }
-        return res.status(200).json({ success: true, filename: pano.filename})
-    }).catch(err => console.log(err))
-}
+		if (!pano) {
+			return res.status(404).json({ success: false, error: `Pano not found` });
+		}
+		return res.status(200).json({ success: true, filename: pano.filename });
+	}).catch(err => console.log(err));
+};
 
-getPanoCoordById = async (req, res) => {
-    await Pano.findOne({ id: req.params.id }, (err, pano) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+const getPanoCoordById = async (req, res) => {
+	await Pano.findOne({ id: req.params.id }, (err, pano) => {
+		if (err) {
+			return res.status(400).json({ success: false, error: err });
+		}
 
-        if (!pano) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Pano not found` })
-        }
-        return res.status(200).json({ success: true, coord: pano.coord })
-    }).catch(err => console.log(err))
-}
+		if (!pano) {
+			return res.status(404).json({ success: false, error: `Pano not found` });
+		}
+		return res.status(200).json({ success: true, coord: pano.coord });
+	}).catch(err => console.log(err));
+};
 
-getPanos = async (req, res) => {
-    await Pano.find({}, (err, panos) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
-        if (!panos.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Pano not found` })
-        }
-        return res.status(200).json({ success: true, data: panos })
-    }).catch(err => console.log(err))
-}
+const getPanos = async (req, res) => {
+	await Pano.find({}, (err, panos) => {
+		if (err) {
+			return res.status(400).json({ success: false, error: err });
+		}
+		if (!panos.length) {
+			return res.status(404).json({ success: false, error: `Pano not found` });
+		}
+		return res.status(200).json({ success: true, data: panos });
+	}).catch(err => console.log(err));
+};
 
-getAllPanosIdAndCoords = async (req, res) => {
-    await Pano.find({}, "id coord",(err, panos) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
-        if (!panos.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Pano not found` })
-        }
-        return res.status(200).json({ success: true, data: panos })
-    }).catch(err => console.log(err))
-}
+const getAllPanosIdAndCoords = async (req, res) => {
+	await Pano.find({}, "id coord", (err, panos) => {
+		if (err) {
+			return res.status(400).json({ success: false, error: err });
+		}
+		if (!panos.length) {
+			return res.status(404).json({ success: false, error: `Pano not found` });
+		}
+		return res.status(200).json({ success: true, data: panos });
+	}).catch(err => console.log(err));
+};
 
-getPanoAllAttrById = async (req, res) => {
-    await Pano.findOne({ id: req.params.id }, (err, pano) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+const getPanoAllAttrById = async (req, res) => {
+	await Pano.findOne({ id: req.params.id }, (err, pano) => {
+		if (err) {
+			return res.status(400).json({ success: false, error: err });
+		}
 
-        if (!pano) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Pano not found` })
-        }
-        return res.status(200).json({success: true, data: {filename: pano.filename ,coord: pano.coord, calibration: pano.calibration} })
-    }).catch(err => console.log(err))
-}
+		if (!pano) {
+			return res.status(404).json({ success: false, error: `Pano not found` });
+		}
+		return res
+			.status(200)
+			.json({
+				success: true,
+				data: {
+					filename: pano.filename,
+					coord: pano.coord,
+					calibration: pano.calibration
+				}
+			});
+	}).catch(err => console.log(err));
+};
 
-updatePanoCalibration = async (req, res) => {
-    const body = req.body
-    if (!body) {
-        return res.status(400).json({
-            success: false,
-            error: 'You must provide a body to update',
-        })
-    }
-    Pano.findOne({ id: req.params.id }, (err, pano) => {
-        if (err) {
-            return res.status(404).json({
-                err,
-                message: 'Pano not found!',
-            })
-        }
-        pano.azimuth = []
-        pano.calibration = body.calibration
-        
-        pano
-            .save()
-            .then(() => {
-                return res.status(200).json({
-                    success: true,
-                    id: pano.id,
-                    message: 'Calibration updated!',
-                })
-            })
-            .catch(error => {
-                return res.status(404).json({
-                    error,
-                    message: 'Calibration not updated!',
-                })
-            })
-    })
-}
+const updatePanoCalibration = async (req, res) => {
+	const body = req.body;
+	if (!body) {
+		return res.status(400).json({
+			success: false,
+			error: "You must provide a body to update"
+		});
+	}
+	Pano.findOne({ id: req.params.id }, (err, pano) => {
+		if (err) {
+			return res.status(404).json({
+				err,
+				message: "Pano not found!"
+			});
+		}
+		pano.azimuth = [];
+		pano.calibration = body.calibration;
+
+		pano
+			.save()
+			.then(() => {
+				return res.status(200).json({
+					success: true,
+					id: pano.id,
+					message: "Calibration updated!"
+				});
+			})
+			.catch(error => {
+				return res.status(404).json({
+					error,
+					message: "Calibration not updated!"
+				});
+			});
+	});
+};
 
 module.exports = {
-    createPano,
-    deletePano,
-    getPanos,
-    getPanoCoordById,
-    getPanoFileNameById,
-    getPanoById,
-    getAllPanosIdAndCoords,
-    getPanoAllAttrById,
-    updatePanoCalibration
-}
+	createPano,
+	deletePano,
+	getPanos,
+	getPanoCoordById,
+	getPanoFileNameById,
+	getPanoById,
+	getAllPanosIdAndCoords,
+	getPanoAllAttrById,
+	updatePanoCalibration
+};

c5351dd03cd4272de93753916255265b60d4c579
Chenglu Li <kabelee92@gmail.com>
2019-09-21 18:38:23 -0500

    Fix the routing issue that pano route must need an id

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,54 @@
-import React from 'react';
-import ReactDOM from "react-dom"
-import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	NavLink,
+	Redirect
+} from "react-router-dom";
 
-import { Container, Header, Content } from './components/layout'
-import GMap from './maps'
-import Pano from './pano'
-import './style/App.css';
+import { Container, Header, Content } from "./components/layout";
+import GMap from "./maps";
+import Pano from "./pano";
+import "./style/App.css";
 
 const routes = [
-  {
-    path: '/maps',
-    title: 'Pano Maps'
-  },
-  {
-    path: '/viewPano/20190724151553',
-    title: 'View Pano'
-  },
-]
+	{
+		path: "/maps",
+		title: "Pano Maps"
+	},
+	{
+		path: "/viewPano",
+		title: "View Pano"
+	}
+];
 
 export const Interface: React.FC = () => {
-  return (
-    <Router>
-      <Container>
-        <Header>
-          {
-            routes.map(r => <NavLink to={r.path} key={r.path} activeClassName='ir-nav-item__active' className="ir-nav-item">{r.title}</NavLink>)
-          }
-        </Header>
-        <Content>
-          <Switch>
-            <Route path="/maps" component={GMap} />
-            <Route path="/viewPano/:id" component={Pano} />
-          </Switch>
-        </Content>
-      </Container>
-    </Router>
-  )
-}
-
-
+	return (
+		<Router>
+			<Container>
+				<Header>
+					{routes.map(r => (
+						<NavLink
+							to={r.path}
+							key={r.path}
+							activeClassName="ir-nav-item__active"
+							className="ir-nav-item"
+						>
+							{r.title}
+						</NavLink>
+					))}
+				</Header>
+				<Content>
+					<Switch>
+						<Route path="/maps" component={GMap} />
+						<Route path="/viewPano/:id" component={Pano} />
+						<Route path="/viewPano" component={Pano} />
+						<Redirect exact from="/" to="maps" />
+					</Switch>
+				</Content>
+			</Container>
+		</Router>
+	);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import * as App from './App';
-import Pano from './pano';
-import GMap from './maps'
-//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 ReactDOM.render(<App.Interface />, document.getElementById('interface'));
 

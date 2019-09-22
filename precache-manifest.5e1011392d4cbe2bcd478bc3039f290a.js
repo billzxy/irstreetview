d23a52c4c8bdbcec0c36ff2ec20fbf3f3a51031e
Chenglu Li <kabelee92@gmail.com>
2019-09-21 19:30:51 -0500

    Updates

---
+++ precache-manifest.5e1011392d4cbe2bcd478bc3039f290a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f234e9e9811fb3ba4dca1802aabc601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d59ebe2c05d724637be",
+    "url": "/static/css/main.1cd23a1f.chunk.css"
+  },
+  {
+    "revision": "1beed4e7f1dcdef8cfd2",
+    "url": "/static/js/2.72f968ba.chunk.js"
+  },
+  {
+    "revision": "7d59ebe2c05d724637be",
+    "url": "/static/js/main.e5b44b34.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "072de3d6f2628a2292440adc5d058765",
+    "url": "/static/media/pano-20190724143458-mx.072de3d6.png"
+  },
+  {
+    "revision": "0732614b480f92d8bbb5fbe12ad85035",
+    "url": "/static/media/pano-20190724143833-mx.0732614b.png"
+  },
+  {
+    "revision": "0c7531e19c279ea5361c8476a867a5e5",
+    "url": "/static/media/pano-20190724145944-mx.0c7531e1.png"
+  },
+  {
+    "revision": "f67813cf582a195b7da651e9e54d2e44",
+    "url": "/static/media/pano-20190724150711-mx.f67813cf.png"
+  },
+  {
+    "revision": "3f9e7726a8bb12346854ae43d66a76f2",
+    "url": "/static/media/pano-20190724151238-mx.3f9e7726.png"
+  },
+  {
+    "revision": "c1e8bcebe6d87073b07b6e6640d9b72b",
+    "url": "/static/media/pano-20190724151553-mx.c1e8bceb.png"
+  },
+  {
+    "revision": "5890e08b7f7847168c4b0e7d7ed22d36",
+    "url": "/static/media/pano-demo.5890e08b.png"
+  },
+  {
+    "revision": "2b939dad255fc3f1c48036a8b12c93d8",
+    "url": "/static/media/wall.2b939dad.jpg"
+  }
+]);
